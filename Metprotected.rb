3bf09b8c798c7540d005396e8e697d1b1bc4a369
Douglas Ferreira <douglinhasprogdev@gmail.com>
2024-05-07 22:52:34 -0300

    Protected

---
+++ Metprotected.rb
@@ -0,0 +1,21 @@
+class Foo
+    def call_protected(instance)
+        instance.bar
+    end
+
+    protected
+
+    def bar
+        puts "protected method"
+    end
+end
+
+instance_1 = Foo.new
+
+instance_2 = Foo.new
+
+instance_1.call_protected(instance_1)
+
+instance_1.call_protected(instance_2)
+
+#O objeto de um usuario consegue acessar o metodo protegido de outro usuario, claro chamando dentro da classe.
